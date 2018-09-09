09eacb131b12bb7c1fe240d49bd56328720f775d
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-09-09 15:24:49 +0200

    Add load balancer service to the installation script

--- resources/systemd/install.sh
+++ resources/systemd/install.sh
@@ -5,6 +5,8 @@ sudo cp \
     arinna-inverter.service \
     arinna-publisher.service \
     arinna-publisher.timer \
+    arinna-load-balancer.service \
+    arinna-load-balancer.timer \
     arinna.target \
     /etc/systemd/system/
 sudo systemctl daemon-reload
@@ -12,7 +14,8 @@ sudo systemctl daemon-reload
 sudo systemctl enable \
     arinna-database.service \
     arinna-inverter.service \
-    arinna-publisher.timer
+    arinna-publisher.timer \
+    arinna-load-balancer.timer
 
 # Uncomment to enable boot loading
 #sudo systemctl enable arinna.target
