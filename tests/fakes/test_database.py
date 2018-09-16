16b438570139b12563b5c6894f14393e5ec1a932
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-09-17 00:33:27 +0200

    Sync contract tests with new FakeDatabase

--- tests/fakes/test_database.py
+++ tests/fakes/test_database.py
@@ -3,6 +3,7 @@
 import influxdb
 import pytest
 from tests.fakes.database import FakeDatabase
+from tests.fakes.database import get_points_with_interval
 
 
 @pytest.fixture
@@ -16,27 +17,23 @@ def sample_database():
 
 
 @pytest.fixture(params=[influxdb.InfluxDBClient, FakeDatabase])
-def database_implementation(request, sample_database):
+def implementation(request, sample_measurement, sample_database):
     database = request.param()
     database.create_database(sample_database)
+    database.write_points(get_points_with_interval([1], sample_measurement),
+                          database=sample_database)
     yield database
     database.drop_database(sample_database)
 
 
 def test_query_mean_without_values(sample_measurement,
                                    sample_database,
-                                   database_implementation):
-    database_implementation.write_points([{
-        'measurement': sample_measurement,
-        'fields': {
-            'value': 1
-        }
-    }], database=sample_database)
+                                   implementation):
     query = 'SELECT MEAN("value") ' \
             'FROM "{}" WHERE time > now() - {}'.format(sample_measurement,
                                                        '0s')
 
-    result = database_implementation.query(query, database=sample_database)
+    result = implementation.query(query, database=sample_database)
 
     with pytest.raises(StopIteration):
         next(result.get_points(sample_measurement))
