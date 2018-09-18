ed7ae035afc12dbad4377007e29b106628a90385
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-09-18 19:00:09 +0200

    Add test for converting percent stored as int to float

--- tests/database_provider_test.py
+++ tests/database_provider_test.py
@@ -125,3 +125,12 @@ def test_bool_from_string_true():
 
 def test_bool_from_string_false():
     assert False is db.bool_from_string('0')
+
+
+def test_percent():
+    assert 0.00 == db.percent(0)
+    assert 0.01 == db.percent(1)
+    assert 0.02 == db.percent(2)
+    assert 0.10 == db.percent(10)
+    assert 0.20 == db.percent(20)
+    assert 1.00 == db.percent(100)
