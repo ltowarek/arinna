2483f96e0e61bcd9b8c41cc1c7f3d12b58a4152a
Lukasz Towarek <lukasz.towarek@gmail.com>
2018-10-27 15:25:52 +0200

    Use mppsolar for parsing responses

--- src/arinna/inverter_provider.py
+++ src/arinna/inverter_provider.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from collections import namedtuple
 import logging
 import arinna.log as log
 import sys
@@ -19,80 +18,7 @@ class InverterSerialAdapter:
         self.serial_adapter = mppsolar.mppUtils(port, baudrate)
 
     def send_command(self, command):
-        raw_response = self.serial_adapter.getResponse(command)
-        return raw_response
-
-    @staticmethod
-    def parse_response(raw_response):
-        ResponseToken = namedtuple('ResponseToken', ['name', 'start', 'end'])
-
-        tokens = [
-            ResponseToken('grid_voltage', 1, 6),
-            ResponseToken('grid_frequency', 7, 11),
-            ResponseToken('ac_output_voltage', 12, 17),
-            ResponseToken('ac_output_frequency', 18, 22),
-            ResponseToken('ac_output_apparent_power', 23, 27),
-            ResponseToken('ac_output_active_power', 28, 32),
-            ResponseToken('output_load_percent', 33, 36),
-            ResponseToken('bus_voltage', 37, 40),
-            ResponseToken('battery_voltage', 41, 46),
-            ResponseToken('battery_charging_current', 47, 50),
-            ResponseToken('battery_capacity', 51, 54),
-            ResponseToken('inverter_heat_sink_temperature', 55, 59),
-            ResponseToken('pv_input_current_for_battery', 60, 64),
-            ResponseToken('pv_input_voltage', 65, 70),
-            ResponseToken('battery_voltage_from_scc', 71, 76),
-            ResponseToken('battery_discharge_current', 77, 82),
-            ResponseToken('is_sbu_priority_version_added', 83, 84),
-            ResponseToken('is_configuration_changed', 84, 85),
-            ResponseToken('is_scc_firmware_updated', 85, 86),
-            ResponseToken('is_load_on', 86, 87),
-            ResponseToken('is_battery_voltage_to_steady_while_charging', 87,
-                          88),
-            ResponseToken('is_charging_on', 88, 89),
-            ResponseToken('is_scc_charging_on', 89, 90),
-            ResponseToken('is_ac_charging_on', 90, 91),
-            ResponseToken('battery_voltage_offset_for_fans_on', 92, 94),
-            ResponseToken('eeprom_version', 95, 97),
-            ResponseToken('pv_charging_power', 98, 103),
-            ResponseToken('is_charging_to_floating_enabled', 104, 105),
-            ResponseToken('is_switch_on', 105, 106),
-            ResponseToken('is_dustproof_installed', 106, 107)
-        ]
-
-        response = {}
-        current_byte_id = 0
-        current_token_id = 0
-        current_token_value = ''
-
-        logger.info('Parsing response')
-        for c in raw_response:
-            if c == '(':
-                logger.debug('Resetting current byte and token ids')
-                current_byte_id = 0
-                current_token_id = 0
-            else:
-                if current_token_id < len(tokens) and \
-                        current_byte_id == tokens[current_token_id].end:
-                    logger.debug('Updating response')
-                    key = tokens[current_token_id].name
-                    value = current_token_value
-                    logger.debug('Key: {}'.format(key))
-                    logger.debug('Value: {}'.format(value))
-                    response[key] = value
-                    current_token_id += 1
-                    logger.debug('Response updated')
-                    logger.debug(
-                        'Increasing token id to: {}'.format(current_token_id))
-                if current_token_id < len(tokens) and \
-                        current_byte_id == tokens[current_token_id].start:
-                    logging.debug('Resetting current token value')
-                    current_token_value = ''
-                current_token_value = current_token_value + c
-            current_byte_id += 1
-        logger.info('Response parsed')
-
-        logger.debug('Parsed response: {}'.format(response))
+        response = self.serial_adapter.getResponseDict(command)
         return response
 
 
@@ -119,9 +45,10 @@ class InverterMQTTPublisher:
         self.mqtt_client = mqtt_client
 
     def publish_response(self, response):
-        for key, value in response.items():
+        for key, status in response.items():
             logger.info('Sending response')
             topic = 'inverter/response/' + key
+            value, unit = status
             logger.info('Topic: {}'.format(topic))
             logger.info('Payload: {}'.format(value))
             self.mqtt_client.publish(topic, value)
@@ -157,9 +84,7 @@ def main():
             logger.info('Waiting for command')
             command = command_queue.get()
             logger.info('Command received: {}'.format(command))
-            raw_response = serial_adapter.send_command(command)
-            logger.info('Raw response: {}'.format(raw_response))
-            response = serial_adapter.parse_response(raw_response)
+            response = serial_adapter.send_command(command)
             logger.info('Response: {}'.format(response))
             mqtt_publisher.publish_response(response)
     except KeyboardInterrupt:
