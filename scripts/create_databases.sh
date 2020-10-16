74f781a12f60c509f89fa9924ae400f922615315
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-10-16 21:18:43 +0200

    Create retention policies

--- scripts/create_databases.sh
+++ scripts/create_databases.sh
@@ -4,3 +4,7 @@ set -e
 curl -POST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE inverter"
 curl -POST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE load"
 curl -POST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE charger"
+
+curl -POST http://localhost:8086/query --data-urlencode "q=CREATE RETENTION POLICY "one_year" ON "inverter" DURATION 52w REPLICATION 1"
+curl -POST http://localhost:8086/query --data-urlencode "q=CREATE RETENTION POLICY "one_year" ON "load" DURATION 52w REPLICATION 1"
+curl -POST http://localhost:8086/query --data-urlencode "q=CREATE RETENTION POLICY "one_year" ON "charger" DURATION 52w REPLICATION 1"
